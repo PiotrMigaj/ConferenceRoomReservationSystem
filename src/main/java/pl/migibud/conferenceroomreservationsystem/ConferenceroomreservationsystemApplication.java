de6172043e1ca6e862212b7f2fcd2c13ff0bde7d
Piotr Migaj <pmigaj@gmail.com>
2022-10-22 10:53:04 +0200

    init commit

---
+++ src/main/java/pl/migibud/conferenceroomreservationsystem/ConferenceroomreservationsystemApplication.java
@@ -0,0 +1,13 @@
+package pl.migibud.conferenceroomreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceroomreservationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceroomreservationsystemApplication.class, args);
+	}
+
+}
